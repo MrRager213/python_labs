6e53d384dc41e7b2324c97343bb3a751aa332a29
MrRager213 <m2502165@edu.misis.ru>
2025-10-28 19:16:52 +0300

    add lab_04

---
+++ src/lab_04/text_report.py
@@ -0,0 +1,46 @@
+from src.lib.io_txt_csv import (
+    read_text,
+    write_csv,
+    frequencies_from_text,
+    sorted_word_counts,
+)
+import argparse
+from src.lib.table import print_summary
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Анализ текста и отчёт в CSV")
+    parser.add_argument("--in", dest="input", default="data/lab04/input.txt")
+    parser.add_argument("--out", dest="output", default="data/report.csv")
+    parser.add_argument("--encoding", dest="encoding", default="utf-8")
+    args = parser.parse_args()
+
+    text = read_text(
+        path=args.input,
+        encoding=args.encoding,
+    )
+    freq = frequencies_from_text(text)
+    data = sorted_word_counts(freq)
+
+    write_csv(
+        header=("word", "count"),
+        rows=data,
+        path=args.output,
+    )
+
+    print_summary(text=text, is_table=True)
+
+
+main()
+
+
+places = [
+    {
+        "title": "awda",
+        "category": "напитки",
+        "subcategory": "",
+        "is_cute": "комфортно",
+        "url": "",
+    },
+    {},
+]
